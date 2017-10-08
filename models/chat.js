a22677429ea6d95cbbd1d5d6253e95984ff8d945
emadehsan <emadulehsan@gmail.com>
2017-10-09 00:08:52 +0500

    add chat model

---
+++ models/chat.js
@@ -0,0 +1,19 @@
+/**
+ * Chat model
+ */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create a schema
+let chatSchema = new Schema({
+	text: String,
+	by: String,
+	on: Date
+})
+
+// the schema is useless so far
+// we need to create a model using it
+const Chat = mongoose.model('Chat', chatSchema)
+
+// make this available to Node applications
+module.exports = Chat;
