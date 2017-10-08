a0ab617a64b6256f69567095106ccc0b13be563d
emadehsan <emadulehsan@gmail.com>
2017-10-08 23:32:53 +0500

    improve vue code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var app = require('express')();
-// var http = require('http').Server(app);
 
 var express = require('express');
 var app = express();
--- public/js/master.js
+++ public/js/master.js
@@ -3,49 +3,35 @@ let socket = io();
 let app = new Vue({
   el: '#app',
   data: {
-    currentUser: 'emad',
+    currentUser: 'anonymous',
     msg: '',
-    messages: [
-      {
-        by: 'emad',
-        text: "What's going on?"
-      },
-      {
-        by: 'abbasi',
-        text: "It's raining."
-      },
-      {
-        by: 'abbasi',
-        text: "How's the climate at your side?"
-      },
-      {
-        by: 'emad',
-        text: "It hasn't rained for a few months. But temperature is quite moderate."
-      }
-    ],
+    messages: [],
     socket: io()
   },
   created () {
-    // init code
+    this.currentUser = prompt("What's your name?")
 
+    // init code
     console.log('created')
-    // receive event listener
-    this.socket.on('chat message', function(obj) {
-        console.log('received: ', obj);
+
+    // register event listener
+    this.socket.on('chat message', (msg) => {
+        console.log('received!');
+        this.messages.push(msg)
         window.scrollTo(0, document.body.scrollHeight);
     })
   },
   methods: {
     send (event) {
-      console.log('sending');
-      let message = this.msg
+      let message = this.msg.trim()
       let by = this.currentUser
 
+      if (message.length === 0) return
+
+      this.msg = ''
+      console.log('sending');
+
       this.socket.emit('chat message', {
-        text: message,
-        by: by
-      })
-      console.log({
         text: message,
         by: by
       })
