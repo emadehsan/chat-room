f9f2051c459a0cb421f8d062399d6a4756957ade
emadehsan <emadulehsan@gmail.com>
2017-10-08 23:13:14 +0500

    vuejs event working

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// var http = require('http').Server(app);
+
+var express = require('express');
+var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use(express.static(__dirname + '/public'));
+
+let server = app.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
+var io = require('socket.io')(server);
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-});
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
 });
--- public/js/master.js
+++ public/js/master.js
@@ -1,7 +1,10 @@
-var app = new Vue({
+let socket = io();
+
+let app = new Vue({
   el: '#app',
   data: {
     currentUser: 'emad',
+    msg: '',
     messages: [
       {
         by: 'emad',
@@ -19,6 +22,25 @@ var app = new Vue({
         by: 'emad',
         text: "It hasn't rained for a few months. But temperature is quite moderate."
       }
-    ]
+    ],
+    socket: io()
+  },
+  methods: {
+    init: function () {
+      // receive event listener
+      // this.socket.on('chat message', function(obj) {
+      //     console.log('received: ', obj);
+      //     window.scrollTo(0, document.body.scrollHeight);
+      // })
+    },
+    send: function (event) {
+      console.log('sending');
+      let message = 'some message'
+      let by = 'someone'
+      // this.socket.emit('chat message', {
+      //   text: message,
+      //   by: by
+      // })
+    }
   }
 })
